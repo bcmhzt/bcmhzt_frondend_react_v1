22b7cea66eb497f52990a5099db67057ea76c8f6
bcmhzt <bcmhzt@gmail.com>
2025-05-20 15:00:12 +0900

    update

--- src/components/VidualBackground.tsx
+++ src/components/VidualBackground.tsx
@@ -1,3 +1,4 @@
+/** 0b16db48 */
 import React, { useState, useEffect } from "react";
 // import BackgroundImage1 from '../assets/background/bcmhzt1.webp';
 // import BackgroundImage2 from '../assets/background/bcmhzt2.webp';
@@ -5,15 +6,26 @@ import React, { useState, useEffect } from "react";
 // import BackgroundImage4 from '../assets/background/bcmhzt4.webp';
 // import BackgroundImage5 from '../assets/background/bcmhzt5.webp';
 
+/* debug */
+let debug = process.env.REACT_APP_DEBUG;
+if (debug === 'true') {
+  console.log("[src/components/VidualBackground.tsx:xx] debug:", debug);
+}
+
 /**
- * [src/components/VidualBackground.js:xx]
- * visual_background	visualBackground	VisualBackground	visual/background
- * 画像はwebpに変換する（軽量化）
+ * 0b16db48 (hash)
+ * [src/components/VidualBackground.tsx:xx]
  * 
+ * type: page | component | hook | context ...
  * 
- * 
- * 
+ * [Order]
+ * - メインビジュアル背景
+ * - 画像はランダムに表示する
+ * - ローディングインジケータを表示
+ * - タイトルとサブタイトルを表示
+ * - タイトルは背景に重ねて表示
  */
+
 const VisualBackground = () => {
   const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
   const [isImageLoaded, setIsImageLoaded] = useState(false);
@@ -66,14 +78,6 @@ const VisualBackground = () => {
         <div className="col-lg-6 mvbg-subright2 mt50">
           ユーザーが<span className="top-text-1">自分の声</span>を発信し、<span className="top-text-2">新しいつながり</span>を築くことで、より<span className="top-text-2">多様で活発</span>な<span className="top-text-3">デジタルコミュニティ</span>を創り上げることを目指しています。
         </div> 
-          
-          {/* <p className="bgbar">
-            <span
-              style={{ fontSize:'20px', fontWeight:'bold' }}>バクムーツ</span>
-              は、ユーザーが自分の声を発信し、新しいつながりを築くことで、より
-            <span style={{ fontSize:'20px', fontWeight:'bold' }}>多様で活発な</span>多様で活発なデジタルコミュニティを創り上げることを目指しています。
-          </p> */}
-        
        </div>
       </div>
     </div>
