082a3dcbc5709e1044621d342b8bb52354f80c95
bcmhzt <bcmhzt@gmail.com>
2025-06-30 18:08:24 +0900

    deploy test

--- _deploy.sh
+++ _deploy.sh
@@ -1,4 +1,22 @@
-git st;
+#!/bin/bash
+
+## Auto deply system
+
+# コミットメッセージを入力
+read -p "Enter commit message: " COMMIT_MSG
+
+echo "Deploying with commit message: $COMMIT_MSG"
+
+git status;
+# ステージングエリアに変更を追加
 git add -A;
-git commit -m "update";
+git commit -m "$COMMIT_MSG";
+git push origin dev;
+
+git checkout test;
+git merge dev;
 git push origin test;
+
+git checkout main;
+git merge test;
+git push origin main;
