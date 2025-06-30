ba7dcbcfb17d44f297225e24f8d6f703e3b78d39
bcmhzt <bcmhzt@gmail.com>
2025-06-30 18:12:40 +0900

    test

--- _deploy.sh
+++ _deploy.sh
@@ -7,6 +7,23 @@ read -p "Enter commit message: " COMMIT_MSG
 
 echo "Deploying with commit message: $COMMIT_MSG"
 
+npm run build;
+echo "Building the project, please wait..."
+while true; do
+  echo -n "."
+  sleep 1
+  if ! ps -C npm > /dev/null; then
+    break
+  fi
+done
+echo -e "\nBuild completed."
+
+read -p "Do you want to proceed with deployment? (Y/n): " PROCEED
+if [[ "$PROCEED" != "Y" && "$PROCEED" != "y" ]]; then
+  echo "Deployment aborted."
+  exit 1
+fi
+
 git status;
 # ステージングエリアに変更を追加
 git add -A;
